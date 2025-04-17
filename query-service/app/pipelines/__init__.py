adbb7b851764e93264c50edfbf460c2aa139508c
dev-nyro <dev.nyro@gmail.com>
2025-04-17 00:00:32 -0500

    Refactor __init__.py: Remove build_rag_pipeline import and update __all__ to include only run_rag_pipeline

--- query-service/app/pipelines/__init__.py
+++ query-service/app/pipelines/__init__.py
@@ -1,4 +1,4 @@
 # ./app/pipelines/__init__.py
-from .rag_pipeline import build_rag_pipeline, run_rag_pipeline
+from .rag_pipeline import run_rag_pipeline
 
-__all__ = ["build_rag_pipeline", "run_rag_pipeline"]
+__all__ = ["run_rag_pipeline"]
