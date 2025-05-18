5ad6a28927907b32231cd1cf6e03d59aa182b2f4
dev-nyro <dev.nyro@gmail.com>
2025-05-18 14:42:39 -0500

    fix: Update Milvus collection name to 'atenex_collection' in configuration and cleanup scripts

--- query-service/app/core/config.py
+++ query-service/app/core/config.py
@@ -17,7 +17,7 @@ POSTGRES_K8S_USER_DEFAULT = "postgres"
 
 # Milvus
 ZILLIZ_ENDPOINT_DEFAULT = "https://in03-0afab716eb46d7f.serverless.gcp-us-west1.cloud.zilliz.com"
-MILVUS_DEFAULT_COLLECTION = "atenex-collection"
+MILVUS_DEFAULT_COLLECTION = "atenex_collection"
 MILVUS_DEFAULT_EMBEDDING_FIELD = "embedding"
 MILVUS_DEFAULT_CONTENT_FIELD = "content"
 MILVUS_DEFAULT_COMPANY_ID_FIELD = "company_id"
